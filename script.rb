265b99d9bdf3a55f9100360cc05576edbcb10f3b
Christian Zamora <christian@diagnostikare.com>
2022-07-09 21:56:22 +0100

    Update script.rb

--- script.rb
+++ script.rb
@@ -4,6 +4,7 @@ run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
 ########################################
 inject_into_file 'Gemfile', before: 'group :development, :test do' do
   <<~RUBY
+    gem 'rails', '6.1.6
     gem 'autoprefixer-rails', '10.2.5'
     gem 'devise'
     gem 'font-awesome-sass'
